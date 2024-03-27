782771f391be3c4fc5ae4d58874499c98736d97d
Kenvin <lnkhanh91@gmail.com>
2024-03-28 00:31:16 +0700

    Fix cannot edit text.

--- examples/next-pages-router/next.config.js
+++ examples/next-pages-router/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 };
 const withTM = require('next-transpile-modules')(['canva-editor']); // pass the modules you would like to see transpiled
 
