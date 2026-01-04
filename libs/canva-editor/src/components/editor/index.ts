c2a4d0dfea7ee4a01df0e95eedbd2599a0e29c4f
Kenvin <lnkhanh91@gmail.com>
2026-01-04 23:45:55 +0700

    Initialize v2

--- libs/canva-editor/src/components/editor/index.ts
+++ libs/canva-editor/src/components/editor/index.ts
@@ -1,5 +1,6 @@
 'use client';
 
+// Get full source code from https://www.canvaclone.com/pricing
 import DesignFrame from './DesignFrame';
 import CanvaEditor from './CanvaEditor';
 import Preview from './Preview';
